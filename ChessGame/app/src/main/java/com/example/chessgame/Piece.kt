c7a0f560f63e3fd4dfb3f33bbb3b1091dee1beb9
Exagor <alexandre.achten@gmail.com>
2022-04-04 22:39:16 +0200

    Premier test

---
+++ ChessGame/app/src/main/java/com/example/chessgame/Piece.kt
@@ -0,0 +1,13 @@
+package com.example.chessgame
+
+abstract class Piece{
+    lateinit var position: String
+    lateinit var color: String
+
+    fun bouger(){
+
+    }
+    fun etreMange(){
+
+    }
+}
