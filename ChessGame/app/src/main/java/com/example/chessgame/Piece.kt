5cee86e949c91b6c7084f6074ef0ea8001d571fc
Exagor <alexandre.achten@gmail.com>
2022-04-04 22:56:33 +0200

    Let's go

--- ChessGame/app/src/main/java/com/example/chessgame/Piece.kt
+++ ChessGame/app/src/main/java/com/example/chessgame/Piece.kt
@@ -1,7 +1,7 @@
 package com.example.chessgame
 
 abstract class Piece{
-    lateinit var position: String
+    lateinit var position: String //pas sur de mettre en string pour la position, plutot mettre un couple
     lateinit var color: String
 
     fun bouger(){
