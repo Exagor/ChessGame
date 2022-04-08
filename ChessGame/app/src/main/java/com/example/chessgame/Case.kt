ea1457ba68ff2d7a63b1b6c54ca42cda19be86cb
Exagor <alexandre.achten@gmail.com>
2022-04-08 14:32:38 +0200

    Téléchargement des images des pieces

--- ChessGame/app/src/main/java/com/example/chessgame/Case.kt
+++ ChessGame/app/src/main/java/com/example/chessgame/Case.kt
@@ -3,6 +3,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
+import androidx.core.content.res.ResourcesCompat.getColor
 
 class Case(var col:Int, var row:Int,piece: Piece?, var x1:Float,var y1: Float,var x2:Float,var y2:Float, view: DrawingView) {
     var case = RectF(x1, y1, x2, y2)
@@ -14,7 +15,7 @@ class Case(var col:Int, var row:Int,piece: Piece?, var x1:Float,var y1: Float,va
 
     fun draw(canvas: Canvas) {
         if ((col+row)%2==0){
-            paint.color = Color.BLUE
+            paint.color= Color.LTGRAY
         }
         else{
             paint.color = Color.BLACK
