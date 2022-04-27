cbf1eefcb56d80a8d75ce67be241fa416f29227d
LoulouLeSang <102982372+LoulouLeSang@users.noreply.github.com>
2022-04-27 16:48:19 +0200

    Update EchecActivity.kt

--- ChessGame/app/src/main/java/com/example/chessgame/EchecActivity.kt
+++ ChessGame/app/src/main/java/com/example/chessgame/EchecActivity.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.widget.ImageView
 import androidx.fragment.app.FragmentContainerView
 
-class MainActivity: AppCompatActivity() {
+class EchecActivity: AppCompatActivity() {
 
     lateinit var drawingView: DrawingView
 
