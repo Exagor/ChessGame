c1235a251cfe70c441e59b763ab7757f5f324f18
alicehrd <alice.houard@gmail.com>
2022-04-29 12:13:06 +0200

    les pions bougent correcteent

--- ChessGame/app/src/main/java/com/example/chessgame/Pion.kt
+++ ChessGame/app/src/main/java/com/example/chessgame/Pion.kt
@@ -6,14 +6,15 @@ class Pion(position: Case, color: String): Piece(position, color) {
         // se déplacer tout droit
         if (position.col == newPos.col) {
             //part de sa position de déprt
-            if (position.row == 1 && color == "black") {
-                println("")
-                return newPos.row == 2 || newPos.row == 3
+            if (position.row == 2 && color == "black") {
+                println("position.row == 1 && color == \"black\"")
+                return newPos.row == 3 || newPos.row == 4
 
-            } else if (position.row == 6 && color == "white") {
-                return newPos.row == 5 || newPos.row == 4
+            } else if (position.row == 7 && color == "white") {
+                println("position.row == 1 && color == \"black\"")
+                return newPos.row == 4 || newPos.row == 5
             } //part d'une case quelconque
-            else return (color == "black" && newPos.row > position.row )|| (color == "white" && newPos.row < position.row )
+            else return (color == "black" && newPos.row  == position.row +1)|| (color == "white" && newPos.row ==  position.row -1 )
         }
         //se déplace en diagonale
         else if ((position.col == newPos.col +1 || position.col ==newPos.col -1) && newPos.piece!!.color != color){
