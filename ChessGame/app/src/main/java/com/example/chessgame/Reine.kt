8e355230c237be53466513a22bd934f83f5eb4a9
LoulouLeSang <102982372+LoulouLeSang@users.noreply.github.com>
2022-04-29 12:14:01 +0200

    Update Reine.kt

--- ChessGame/app/src/main/java/com/example/chessgame/Reine.kt
+++ ChessGame/app/src/main/java/com/example/chessgame/Reine.kt
@@ -1,10 +1,28 @@
 package com.example.chessgame
 
+import kotlin.math.abs
+
 class Reine(position: Case, color: String): Piece(position, color) {
     override var image = if (color == "white") R.drawable.queen_white else R.drawable.queen_black
-    override fun bouger(newPos: Case, cases : MutableList<Case>): Boolean{
-        var fait = false
-        return fait
+    override fun bouger(newPos: Case, cases: List<Case>): Boolean {
+        if (abs(newPos.row - position.row) == abs(newPos.col - position.col) && isDiagonalFree(position, newPos, cases)) {
+            if (newPos.piece == null) {
+                return true
+            }
+            else if (newPos.piece!!.color != color) {
+                newPos.piece!!.mourir()
+            }
+
+        }
+        else if((abs(newPos.row - position.row) == 0 && isHorizontalFree(position,newPos,cases)) or (abs(newPos.col - position.col) == 0 && isVerticalFree(position,newPos,cases)) ){
+            if (newPos.piece == null) {
+                return true
+            }
+            else if (newPos.piece!!.color != color) {
+                newPos.piece!!.mourir()
+            }
+        }
+        return false
     }
 
     override fun mourir() {
