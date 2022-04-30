abe839aa0f3c7f9811287a284b872483e681b22a
alicehrd <alice.houard@gmail.com>
2022-04-30 18:10:28 +0200

    suppression des fonctions non utilisées

--- ChessGame/app/src/main/java/com/example/chessgame/Board.kt
+++ ChessGame/app/src/main/java/com/example/chessgame/Board.kt
@@ -124,41 +124,6 @@ class Board(var boardHauteur: Float, var boardDebut: Float,  var width: Float, v
         cimetiere.ajouterPiece(piece!!)
     }
 
-    fun isDiagonalFree( from: Case,  to : Case): Boolean{
-        if (abs(from.col - to.col) != abs(from.row - to.row)) return false // pas une diagonale
-        val ecart = abs(from.col - to.col) - 1
-        for (i in 1..ecart) {
-            val nextCol = if (to.col > from.col) from.col + i else from.col - i
-            val nextRow = if (to.row > from.row) from.row + i else from.row - i
-            if (cases[(nextRow-1)*8 + nextCol].piece != null) {
-                return false
-            }
-        }
-        return true
-    }
-    fun isVerticalFree(from: Case, to: Case): Boolean{
-        if (from.col != to.col) return false
-        val ecart = abs(from.row - to.row) - 1
-        if (ecart == 0 ) return true
-        for (i in 1..ecart) {
-            val nextRow = if (to.row > from.row) from.row + i else from.row - i
-            if (cases[(nextRow-1)*8 + from.col].piece != null) {
-                return false
-            }
-        }
-        return true
-    }
-    fun isHorizontallyFree(from: Case, to: Case): Boolean {
-        if (from.row != to.row) return false
-        val ecart = abs(from.col - to.col) - 1
-        if (ecart == 0 ) return true
-        for (i in 1..ecart) {
-            val nextCol = if (to.col > from.col) from.col + i else from.col - i
-            if (cases[(from.row-1)*8 + nextCol].piece != null) {
-                return false
-            }
-        }
-        return true
-    }
+
 
 }
